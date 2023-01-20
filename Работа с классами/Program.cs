84690f0a018f077178888a5bf88ee2f7a2993f3b
Huawei <Huawei@LAPTOP-5LRHA01L>
2023-01-20 11:24:32 +0300

    Добавьте файлы проекта.

---
+++ Работа с классами/Program.cs
@@ -0,0 +1,10 @@
+﻿using Работа_с_классами;
+
+Random rand = new Random();
+Triangle[] triangles = new Triangle[5];
+for (int i = 0; i < triangles.Length; i++)
+{
+    triangles[i] = new Triangle(rand.Next(1, 90), rand.Next(1, 90));
+    Console.WriteLine(triangles[i]);
+}
+
---
+++ Работа с классами/Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Работа_с_классами
+{
+    internal class Triangle
+    {
+        public Triangle(double angle, double rand)
+        {
+            Angle1 = angle;
+            Angle2 = 180 - angle - rand;
+            Angle3 = 180 - angle - (180 - angle - rand);
+
+        }
+
+        public double Angle1 { get; set; }                          
+        public double Angle2 { get; set; }
+        public double Angle3 { get; set; }
+
+        public override string ToString()
+        {
+            return "(" +Angle1 + "+" + Angle2 + "+" + Angle3+ ")";
+        }
+    }
+    
+
+
+
+}
